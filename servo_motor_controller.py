9e0c2cf903fd71aa83fb4b9ed730a5f72eea9e3c
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2016-05-03 23:15:49 -0400

    new file to execute the channel 2 commands.

---
+++ servo_motor_controller.py
@@ -0,0 +1,29 @@
+import time
+import maestro
+# servo 0 is left/right
+# servo 1 is up/down
+
+# about 5 clicks per full motion
+# 1040 for left/right  + is left, - is right.
+# 800 for up/down  + is up, - is down.
+  
+try:
+  servo = maestro.Controller()
+
+  servo.setRange(0,3000,8200)
+  servo.setRange(1,4000,8000)
+  servo.setAccel(1,6)
+  
+  # open channel 2 file.  2 = servos, 1 = dc motors
+  f = open('/etc/ramdisk/cmd2.txt', 'r+')
+  cmd = f.readline()
+
+  if cmd == "up":
+    x = servo.getPosition(1) - 800
+    servo.setTarget(1,x)
+  elif cmd == "down":
+    x = servo.getPosition(1) - 800
+    servo.setTarget(1,x)
+
+finally:
+  servo.close
