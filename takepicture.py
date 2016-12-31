7f46a1a1a1d00897d88d002c43699d013ce45e9f
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2016-12-30 23:12:10 -0500

    Update takepicture.py

--- takepicture.py
+++ takepicture.py
@@ -8,22 +8,33 @@ import os.path
 import sys
 import os
 
-turn_off_face_fname = "/var/www/html/turn_off_face.txt"
-done = False
+try:
+	import socket
+	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+	## Create an abstract socket, by prefixing it with null. 
+	s.bind( '\0take_pictures_lock') 
 
-if __name__ == '__main__':
+except socket.error, e:
+	error_code = e.args[0]
+	error_string = e.args[1]
+	print "Process already running (%d:%s ). Exiting" % ( error_code, error_string) 
+	sys.exit (0)
+	
+else:
+	turn_off_face_fname = "/var/www/html/turn_off_face.txt"
+	done = False
 
 	webcam = cv2.VideoCapture(-1)
 	webcam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
 	webcam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 
-while not done:
+	while not done:
 
-	if not os.path.isfile(turn_off_face_fname):
-		webcam.release()
-		done=True
-		exit()
+		if not os.path.isfile(turn_off_face_fname):
+			webcam.release()
+			done=True
+			exit()
 
-	aframe = webcam.read()[1]
-	cv2.imwrite("/mnt/ramdisk/image.jpg",aframe)
-	time.sleep(0.1)
+		aframe = webcam.read()[1]
+		cv2.imwrite("/mnt/ramdisk/image.jpg",aframe)
+		time.sleep(0.1)
