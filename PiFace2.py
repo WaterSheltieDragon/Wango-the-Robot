c2a99925dfbecc4b94d7a7e1323034cd8a10f83a
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2016-12-31 10:13:41 -0500

    Update PiFace2.py

--- PiFace2.py
+++ PiFace2.py
@@ -97,35 +97,6 @@ def P1():	# Process 1 controlles servo 1 using same logic as above
 		if _Servo1CP == _Servo1DP:
 			_Servo1S = 1
 
-
-if __name__ == '__main__':
-
-	servo = maestro.Controller()
-
-	servo.setAccel(1,8)
-	servo.setAccel(0,8)
-	servo.setRange(0,_Servo0LL,_Servo0UL)
-	servo.setRange(1,_Servo1LL,_Servo1UL)
-	servo.setTarget(0,(_Servo0UL - _Servo0LL)/2+_Servo0LL)
-	servo.setTarget(1,(_Servo1UL - _Servo1LL)/2+_Servo1LL)
-
-
-	webcam = cv2.VideoCapture(-1)                           # Get ready to start getting images from the webcam
-	webcam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)         # I have found this to be about the highest-
-	webcam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)        #       resolution you'll want to attempt on the pi
-
-	frontalface = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")         # frontal face pattern detection
-	profileface = cv2.CascadeClassifier("haarcascade_profileface.xml")              # side face pattern detection
-
-	p_p0 = Process(target=P0, args=())	# Start the subprocesses
-        p_p0.start()
-	p_p1 = Process(target=P1, args=())	#
-        p_p1.start()
-	time.sleep(1)				# Wait for them to start
-	print p_p0, p_p0.is_alive()
-
-#====================================================================================================
-
 def CamRight( distance, speed ):		# To move right, we are provided a distance to move and a speed to move.
 	global _Servo0CP			# We Global it so  everyone is on the same page about where the servo is...
 	if not Servo0CP.empty():		# Read it's current position given by the subprocess(if it's avalible)-
@@ -171,131 +142,174 @@ def CamUp(distance, speed):			# Same logic as above
 	Servo1DP.put(_Servo1DP)
 	Servo1S.put(speed)
 	return;
-
-
-
 #============================================================================================================
 
-done = False
-cnt = 0
-while not done:
 
-	faceFound = False	# This variable is set to true if, on THIS loop a face has already been found
-				# We search for a face three diffrent ways, and if we have found one already-
-				# there is no reason to keep looking.
+if __name__ == '__main__':
 
-        if os.path.isfile(turn_off_face_fname):
+	
+	try:
+		import socket
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		## Create an abstract socket, by prefixing it with null. 
+		s.bind( '\0find_face_lock') 
+
+	except socket.error, e:
+		error_code = e.args[0]
+		error_string = e.args[1]
+		print "Process already running (%d:%s ). Exiting" % ( error_code, error_string) 
+		sys.exit (0)
+
+	else:
 		try:
-			print "1"
-			Servo0CP.close()
-			Servo1CP.close()
-			Servo0DP.close()
-			Servo1DP.close()
-			Servo0S.close()
-			Servo1S.close()
-			p_p0.terminate()
-			time.sleep(0.1)
-			p_p1.terminate()
-			time.sleep(0.1)
-			p_p0.join()
-			p_p1.join()
-			webcam.release()
-			servo.close()
-		finally:
-			print "5"
+			servo = maestro.Controller()
 
-		exit()
-		done = True
-
-	if not faceFound:
-		if lastface == 0 or lastface == 1:
-			aframe = webcam.read()[1]	# there seems to be an issue in OpenCV or V4L or my webcam-
-			aframe = webcam.read()[1]	# 	driver, I'm not sure which, but if you wait too long,
-			aframe = webcam.read()[1]	#	the webcam consistantly gets exactly five frames behind-
-			aframe = webcam.read()[1]	#	realtime. So we just grab a frame five times to ensure-
-			aframe = webcam.read()[1]	#	we have the most up-to-date image.
-			fface = frontalface.detectMultiScale(aframe,1.3,4,(cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),(60,60))
-			if fface != ():			# if we found a frontal face...
-				lastface = 1		# set lastface 1 (so next loop we will only look for a frontface)
-				for f in fface:		# f in fface is an array with a rectangle representing a face
-					faceFound = True
-					face = f
-
-	if not faceFound:				# if we didnt find a face yet...
-		if lastface == 0 or lastface == 2:	# only attempt it if we didn't find a face last loop or if-
-			aframe = webcam.read()[1]	# 	THIS method was the one who found it last loop
-			aframe = webcam.read()[1]
-			aframe = webcam.read()[1]	# again we grab some frames, things may have gotten stale-
-			aframe = webcam.read()[1]	# since the frontalface search above
-			aframe = webcam.read()[1]
-			pfacer = profileface.detectMultiScale(aframe,1.3,4,(cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),(80,80))
-			if pfacer != ():		# if we found a profile face...
-				lastface = 2
-				for f in pfacer:
-					faceFound = True
-					face = f
-
-	if not faceFound:				# a final attempt
-		if lastface == 0 or lastface == 3:	# this is another profile face search, because OpenCV can only-
-			aframe = webcam.read()[1]	#	detect right profile faces, if the cam is looking at-
-			aframe = webcam.read()[1]	#	someone from the left, it won't see them. So we just...
-			aframe = webcam.read()[1]
-			aframe = webcam.read()[1]
-			aframe = webcam.read()[1]
-			cv2.flip(aframe,1,aframe)	#	flip the image
-			pfacel = profileface.detectMultiScale(aframe,1.3,4,(cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),(80,80))
-			if pfacel != ():
-				lastface = 3
-				for f in pfacel:
-					faceFound = True
-					face = f
-
-	if not faceFound:		# if no face was found...-
-		lastface = 0		# 	the next loop needs to know
-		face = [0,0,0,0]	# so that it doesn't think the face is still where it was last loop
+			servo.setAccel(1,8)
+			servo.setAccel(0,8)
+			servo.setRange(0,_Servo0LL,_Servo0UL)
+			servo.setRange(1,_Servo1LL,_Servo1UL)
+			servo.setTarget(0,(_Servo0UL - _Servo0LL)/2+_Servo0LL)
+			servo.setTarget(1,(_Servo1UL - _Servo1LL)/2+_Servo1LL)
 
 
-	x,y,w,h = face
-	Cface = [(w/2+x),(h/2+y)]	# we are given an x,y corner point and a width and height, we need the center
+			webcam = cv2.VideoCapture(-1)                           # Get ready to start getting images from the webcam
+			webcam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)         # I have found this to be about the highest-
+			webcam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)        #       resolution you'll want to attempt on the pi
 
-	img_out = aframe.copy()
+			frontalface = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")         # frontal face pattern detection
+			profileface = cv2.CascadeClassifier("haarcascade_profileface.xml")              # side face pattern detection
 
-	if Cface[0] != 0:		# if the Center of the face is not zero (meaning no face was found)
+			p_p0 = Process(target=P0, args=())	# Start the subprocesses
+			p_p0.start()
+			p_p1 = Process(target=P1, args=())	#
+			p_p1.start()
+			time.sleep(1)				# Wait for them to start
+			print p_p0, p_p0.is_alive()
 
- 		draw_rect(img_out,face,(0,255,0))
 
-                print str(Cface[0]) + "," + str(Cface[1])
+			done = False
+			cnt = 0
+			while not done:
+				
+				time.sleep(0.1)
+				
+				faceFound = False	# This variable is set to true if, on THIS loop a face has already been found
+							# We search for a face three diffrent ways, and if we have found one already-
+							# there is no reason to keep looking.
 
-		if Cface[0] > 200:	# The camera is moved diffrent distances and speeds depending on how far away-
-			CamLeft(200,3)	#	from the center of that axis it detects a face
-		elif Cface[0] > 190:	#
-			CamLeft(100,2)	#
-		elif Cface[0] > 180:	#
-			CamLeft(50,1)	#
+				if os.path.isfile(turn_off_face_fname):
+					try:
+						print "Closing servos."
+						Servo0CP.close()
+						Servo1CP.close()
+						Servo0DP.close()
+						Servo1DP.close()
+						Servo0S.close()
+						Servo1S.close()
+						p_p0.terminate()
+						time.sleep(0.1)
+						p_p1.terminate()
+						time.sleep(0.1)
+						p_p0.join()
+						p_p1.join()
+						webcam.release()
+						servo.close()
+						print "Completed normal shutdown of PiFace."
+					finally:
+						print "Done."
 
-		if Cface[0] < 120:	# and diffrent dirrections depending on what side of center if finds a face.
-			CamRight(200,3)
-		elif Cface[0] < 130:
-			CamRight(100,2)
-		elif Cface[0] < 140:
-			CamRight(50,1)
+					exit()
+					done = True
 
-		if Cface[1] > 170:	# and moves diffrent servos depending on what axis we are talking about.
-			CamUp(300,1)
-		elif Cface[1] > 150:
-			CamUp(300,2)
-		elif Cface[1] > 140:
-			CamUp(200,3)
+				if not faceFound and not done:
+					if lastface == 0 or lastface == 1:
+						aframe = webcam.read()[1]	# there seems to be an issue in OpenCV or V4L or my webcam-
+						aframe = webcam.read()[1]	# 	driver, I'm not sure which, but if you wait too long,
+						aframe = webcam.read()[1]	#	the webcam consistantly gets exactly five frames behind-
+						aframe = webcam.read()[1]	#	realtime. So we just grab a frame five times to ensure-
+						aframe = webcam.read()[1]	#	we have the most up-to-date image.
+						fface = frontalface.detectMultiScale(aframe,1.3,4,(cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),(60,60))
+						if fface != ():			# if we found a frontal face...
+							lastface = 1		# set lastface 1 (so next loop we will only look for a frontface)
+							for f in fface:		# f in fface is an array with a rectangle representing a face
+								faceFound = True
+								face = f
 
-		if Cface[1] < 80:
-			CamDown(400,3)
-		elif Cface[1] < 90:
-			CamDown(300,2)
-		elif Cface[1] < 110:
-			CamDown(200,1)
+					if not faceFound:				# if we didnt find a face yet...
+						if lastface == 0 or lastface == 2:	# only attempt it if we didn't find a face last loop or if-
+							aframe = webcam.read()[1]	# 	THIS method was the one who found it last loop
+							aframe = webcam.read()[1]
+							aframe = webcam.read()[1]	# again we grab some frames, things may have gotten stale-
+							aframe = webcam.read()[1]	# since the frontalface search above
+							aframe = webcam.read()[1]
+							pfacer = profileface.detectMultiScale(aframe,1.3,4,(cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),(80,80))
+							if pfacer != ():		# if we found a profile face...
+								lastface = 2
+								for f in pfacer:
+									faceFound = True
+									face = f
 
-	cnt = cnt + 1
-	if cnt == 10:
-		cnt = 0
-		cv2.imwrite("/var/www/html/image.jpg",img_out)
+					if not faceFound:				# a final attempt
+						if lastface == 0 or lastface == 3:	# this is another profile face search, because OpenCV can only-
+							aframe = webcam.read()[1]	#	detect right profile faces, if the cam is looking at-
+							aframe = webcam.read()[1]	#	someone from the left, it won't see them. So we just...
+							aframe = webcam.read()[1]
+							aframe = webcam.read()[1]
+							aframe = webcam.read()[1]
+							cv2.flip(aframe,1,aframe)	#	flip the image
+							pfacel = profileface.detectMultiScale(aframe,1.3,4,(cv2.cv.CV_HAAR_DO_CANNY_PRUNING + cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT + cv2.cv.CV_HAAR_DO_ROUGH_SEARCH),(80,80))
+							if pfacel != ():
+								lastface = 3
+								for f in pfacel:
+									faceFound = True
+									face = f
+
+					if not faceFound:		# if no face was found...-
+						lastface = 0		# 	the next loop needs to know
+						face = [0,0,0,0]	# so that it doesn't think the face is still where it was last loop
+
+
+					x,y,w,h = face
+					Cface = [(w/2+x),(h/2+y)]	# we are given an x,y corner point and a width and height, we need the center
+
+					img_out = aframe.copy()
+
+					if Cface[0] != 0:		# if the Center of the face is not zero (meaning no face was found)
+
+						draw_rect(img_out,face,(0,255,0))
+
+						print str(Cface[0]) + "," + str(Cface[1])
+
+						if Cface[0] > 200:	# The camera is moved diffrent distances and speeds depending on how far away-
+							CamLeft(200,3)	#	from the center of that axis it detects a face
+						elif Cface[0] > 190:	#
+							CamLeft(100,2)	#
+						elif Cface[0] > 180:	#
+							CamLeft(50,1)	#
+
+						if Cface[0] < 120:	# and diffrent dirrections depending on what side of center if finds a face.
+							CamRight(200,3)
+						elif Cface[0] < 130:
+							CamRight(100,2)
+						elif Cface[0] < 140:
+							CamRight(50,1)
+
+						if Cface[1] > 170:	# and moves diffrent servos depending on what axis we are talking about.
+							CamUp(300,1)
+						elif Cface[1] > 150:
+							CamUp(300,2)
+						elif Cface[1] > 140:
+							CamUp(200,3)
+
+						if Cface[1] < 80:
+							CamDown(400,3)
+						elif Cface[1] < 90:
+							CamDown(300,2)
+						elif Cface[1] < 110:
+							CamDown(200,1)
+
+					cnt = cnt + 1
+					if cnt == 10:
+						cnt = 0
+						cv2.imwrite("/var/www/html/image.jpg",img_out)
 
