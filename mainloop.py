d86f72835055cf30b5eafb50987c8cf2309d8405
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2016-12-31 13:57:15 -0500

    Update mainloop.py

--- mainloop.py
+++ mainloop.py
@@ -1,17 +1,27 @@
-a = 0
 import sys
+from subprocess import call
 
 try:
     import socket
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     ## Create an abstract socket, by prefixing it with null. 
-    s.bind( '\0postconnect_gateway_notify_lock') 
-    print "done."
-    while True:
-      a = 0
+    s.bind( '\0mainloop_lock') 
       
 except socket.error, e:
     error_code = e.args[0]
     error_string = e.args[1]
     print "Process already running (%d:%s ). Exiting" % ( error_code, error_string) 
     sys.exit (0)
+    
+else:
+    try:
+        myfile = open('/etc/ramdisk/cmd1.txt', 'r')
+        while True:
+            time.sleep(0.1)
+            cmd = myfile.readline()
+            if cmd == 'left':
+                call(["moveleft.py", ""])
+        
+    except:
+        myfile.close()
+    
