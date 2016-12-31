d9e1fc44c673d175d115410fe1c42fe39293a44d
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2016-12-31 12:55:35 -0500

    Update PiFace2.py

--- PiFace2.py
+++ PiFace2.py
@@ -53,20 +53,21 @@ def P0():	# Process 0 controlles servo0
 		if not Servo0S.empty():			# Constantly read read Servo0S in case the main process-
 			_Servo0S = Servo0S.get()	# 	has updated it, the higher the speed value, the shorter-
 			speed = .1 / _Servo0S		# 	the wait between loops will be, so the servo moves faster
-		if _Servo0CP < _Servo0DP:					# if Servo0CP less than Servo0DP
-			_Servo0CP += 10						# incriment Servo0CP up by one
-			Servo0CP.put(_Servo0CP)					# move the servo that little bit
-			servo.setTarget(0,_Servo0CP)	#
-			if not Servo0CP.empty():				# throw away the old Servo0CP value,-
-				trash = Servo0CP.get()				# 	it's no longer relevent
-		if _Servo0CP > _Servo0DP:					# if Servo0CP greater than Servo0DP
-			_Servo0CP -= 10						# incriment Servo0CP down by one
-			Servo0CP.put(_Servo0CP)					# move the servo that little bit
-			servo.setTarget(0,_Servo0CP)	#
-			if not Servo0CP.empty():				# throw away the old Servo0CP value,-
-				trash = Servo0CP.get()				# 	it's no longer relevent
-		if _Servo0CP == _Servo0DP:	        # if all is good,-
-			_Servo0S = 1		        # slow the speed; no need to eat CPU just waiting
+		if not servo.getMovingState():
+			if _Servo0CP < _Servo0DP:					# if Servo0CP less than Servo0DP
+				_Servo0CP += 10						# incriment Servo0CP up by one
+				Servo0CP.put(_Servo0CP)					# move the servo that little bit
+				servo.setTarget(0,_Servo0CP)	#
+				if not Servo0CP.empty():				# throw away the old Servo0CP value,-
+					trash = Servo0CP.get()				# 	it's no longer relevent
+			if _Servo0CP > _Servo0DP:					# if Servo0CP greater than Servo0DP
+				_Servo0CP -= 10						# incriment Servo0CP down by one
+				Servo0CP.put(_Servo0CP)					# move the servo that little bit
+				servo.setTarget(0,_Servo0CP)	#
+				if not Servo0CP.empty():				# throw away the old Servo0CP value,-
+					trash = Servo0CP.get()				# 	it's no longer relevent
+			if _Servo0CP == _Servo0DP:	        # if all is good,-
+				_Servo0S = 1		        # slow the speed; no need to eat CPU just waiting
 			
 
 def P1():	# Process 1 controlles servo 1 using same logic as above
@@ -82,20 +83,21 @@ def P1():	# Process 1 controlles servo 1 using same logic as above
 		if not Servo1S.empty():
 			_Servo1S = Servo1S.get()
 			speed = .1 / _Servo1S
-		if _Servo1CP < _Servo1DP:
-			_Servo1CP += 30
-			Servo1CP.put(_Servo1CP)
-			servo.setTarget(1,_Servo1CP)
-			if not Servo1CP.empty():
-				trash = Servo1CP.get()
-		if _Servo1CP > _Servo1DP:
-			_Servo1CP -= 30
-			Servo1CP.put(_Servo1CP)
-			servo.setTarget(1,_Servo1CP)
-			if not Servo1CP.empty():
-				trash = Servo1CP.get()
-		if _Servo1CP == _Servo1DP:
-			_Servo1S = 1
+		if not servo.getMovingState():
+			if _Servo1CP < _Servo1DP:
+				_Servo1CP += 30
+				Servo1CP.put(_Servo1CP)
+				servo.setTarget(1,_Servo1CP)
+				if not Servo1CP.empty():
+					trash = Servo1CP.get()
+			if _Servo1CP > _Servo1DP:
+				_Servo1CP -= 30
+				Servo1CP.put(_Servo1CP)
+				servo.setTarget(1,_Servo1CP)
+				if not Servo1CP.empty():
+					trash = Servo1CP.get()
+			if _Servo1CP == _Servo1DP:
+				_Servo1S = 1
 
 def CamRight( distance, speed ):		# To move right, we are provided a distance to move and a speed to move.
 	global _Servo0CP			# We Global it so  everyone is on the same page about where the servo is...
