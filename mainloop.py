286f8c31b40cc890ebda8d95cae8f6df56f82de7
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2016-12-30 21:17:13 -0500

    Create mainloop.py

---
+++ mainloop.py
@@ -0,0 +1,14 @@
+try:
+    import socket
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    ## Create an abstract socket, by prefixing it with null. 
+    s.bind( '\0postconnect_gateway_notify_lock') 
+    print "done."
+    while True:
+      #nothing
+      
+except socket.error, e:
+    error_code = e.args[0]
+    error_string = e.args[1]
+    print "Process already running (%d:%s ). Exiting" % ( error_code, error_string) 
+    sys.exit (0)
