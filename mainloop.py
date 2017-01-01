3856b5fd331747342f8b28d78ccba40b0abf4e5d
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2017-01-01 14:01:35 -0500

    Update mainloop.py

--- mainloop.py
+++ mainloop.py
@@ -25,8 +25,7 @@ else:
                 if os.path.isfile('/mnt/ramdisk/cmd-' + c + '.txt'):
                     os.remove('/mnt/ramdisk/cmd-' + c + '.txt')
                     call(["sudo python " + c + ".py &", ""])
-    except:
-        Exception as e:
+    except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         print(exc_type, fname, exc_tb.tb_lineno)
