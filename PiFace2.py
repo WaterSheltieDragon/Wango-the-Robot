59b3148cc8f8925f8f9016f521853aaaba742c78
WaterSheltieDragon <WaterSheltieDragon@users.noreply.github.com>
2017-01-27 12:32:13 -0500

    Update PiFace2.py

--- PiFace2.py
+++ PiFace2.py
@@ -105,7 +105,7 @@ def P1():	# Process 1 controlles servo 1 : up and down
 					if not Servo1CP.empty():
 						trash = Servo1CP.get()
 				if _Servo1CP > _Servo1DP:
-					_Servo1CP -= 10	* _Servo0S
+					_Servo1CP -= 10	* _Servo1S
 					Servo1CP.put(_Servo1CP)
 					servo.setTarget(1,_Servo1CP)
 					if not Servo1CP.empty():
